8e21f76826b9f8d7a7fe0eea800a3309d44f99c2
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-05-08 23:33:19 +0530

    tried to manipulate things using javascript

---
+++ demo.js
@@ -0,0 +1,6 @@
+//console.dir(document)
+document.title='hello';
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 4px #000';
+var h=document.getElementsByClassName('title');
+h[0].style.color='Green';
